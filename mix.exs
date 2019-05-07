1851dca390a797f457f29c9e293b40c124b70bd0
Danilo <danilo.lima@sumup.com>
2019-05-06 23:42:27 -0300

    Added dependencies

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule Prepend.MixProject do
       app: :prepend,
       version: "0.1.0",
       elixir: "~> 1.6",
+      escript: [main_module: Prepend.CLI],
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -14,15 +16,17 @@ defmodule Prepend.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :stream_data]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:stream_data, "~> 0.4.2", only: [:dev, :test], runtime: false},
+      {:credo, "~> 0.9.0-rc1", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 0.5", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5", only: :dev, runtime: false}
     ]
   end
 end
