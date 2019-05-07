a30863e3a89c5addc874d69a60e55a066d14e262
Danilo <danilo.lima@sumup.com>
2019-05-06 23:26:35 -0300

    Added @doc and @spec

--- lib/prepend.ex
+++ lib/prepend.ex
@@ -15,4 +15,18 @@ defmodule Prepend do
   def hello do
     :world
   end
+
+  @doc """
+  Prepend the given string to each line of a stream
+  and returns a stream of the resulting lines.
+
+  ## Example:
+
+  iex> ["one", "two"] |> Prepend.stream_lines("$ ") |> Enum.to_list
+  ["$ one", "$ two"]
+
+  """
+  @spec stream_lines(Enumerable.t(), String.t()) :: Enumerable.t()
+  def stream_lines(in_stream, string) do
+    in_stream |> Stream.map(&(string <> &1)) end
 end
