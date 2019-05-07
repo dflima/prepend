d074ea4b71b5b6145f427659bfa0260ba6479bac
Danilo <danilo.lima@sumup.com>
2019-05-06 23:26:44 -0300

    Added tests

--- test/prepend_test.exs
+++ test/prepend_test.exs
@@ -1,8 +1,34 @@
 defmodule PrependTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Prepend
 
   test "greets the world" do
     assert Prepend.hello() == :world
+  end
+
+  test "prepends string to lines" do
+    expected = ["$ one", "$ two"]
+
+    received = ["one", "two"] |> Prepend.stream_lines("$ ")
+               |> Enum.to_list
+
+    assert received == expected
+  end
+
+  test "handles multiple words" do
+    expected = ["I like ice cream", "I like pizza", "I like cats"]
+
+    received = ["ice cream", "pizza", "cats"]
+               |> Prepend.stream_lines("I like ")
+               |> Enum.to_list
+
+    assert received == expected
+  end
+
+  @tag timeout: 300000
+  property "streams" do
+    check all lines <- list_of(string(:printable)), prefix <- string(:printable), max_runs <- 1000 do
+      lines |> Prepend.stream_lines(prefix) |> Stream.each(&String.starts_with?(&1, prefix))
+    end
   end
 end
