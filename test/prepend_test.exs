a87e6f28bd0dd7e1a542a6b65fc5af647a9f4f2e
Danilo <danilo.lima@sumup.com>
2019-10-09 11:12:25 -0300

    Code formatted

--- test/prepend_test.exs
+++ test/prepend_test.exs
@@ -10,8 +10,10 @@ defmodule PrependTest do
   test "prepends string to lines" do
     expected = ["$ one", "$ two"]
 
-    received = ["one", "two"] |> Prepend.stream_lines("$ ")
-               |> Enum.to_list
+    received =
+      ["one", "two"]
+      |> Prepend.stream_lines("$ ")
+      |> Enum.to_list()
 
     assert received == expected
   end
@@ -19,16 +21,17 @@ defmodule PrependTest do
   test "handles multiple words" do
     expected = ["I like ice cream", "I like pizza", "I like cats"]
 
-    received = ["ice cream", "pizza", "cats"]
-               |> Prepend.stream_lines("I like ")
-               |> Enum.to_list
+    received =
+      ["ice cream", "pizza", "cats"]
+      |> Prepend.stream_lines("I like ")
+      |> Enum.to_list()
 
     assert received == expected
   end
 
-  @tag timeout: 300000
+  @tag timeout: 300_000
   property "streams" do
-    check all lines <- list_of(string(:printable)), prefix <- string(:printable), max_runs: 1000 do
+    check all(lines <- list_of(string(:printable)), prefix <- string(:printable), max_runs: 1000) do
       lines |> Prepend.stream_lines(prefix) |> Stream.each(&String.starts_with?(&1, prefix))
     end
   end
